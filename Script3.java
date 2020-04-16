fe5cb8cbe1e82a4188faf05a7cd02f23bd55463c
Your Name <you@example.com>
2020-04-16 13:32:04 +0530

    want to commit

---
+++ Script3.java
@@ -0,0 +1,14 @@
+package PracticeJava;
+
+public class Script3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println ("this is test phase 1");
+		System.out.println ("this is test phase 2");
+		System.out.println ("this is test phase 3");
+
+	}
+
+}
